efc6dc6ddb2c38b803d882d2097a1bef9be601d5
Prince Arora <77785932+princearoragithub@users.noreply.github.com>
2024-02-10 14:43:11 +0530

    mainnet constants

--- server/server/const.py
+++ server/server/const.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 import os
 
 from bson import Decimal128
+from server.utils import format_address
 
 
 class Collection:
@@ -17,26 +18,26 @@ class Collection:
     TOKENS_DAY_DATA = 'tokens_day_data'
     TOKENS_HOUR_DATA = 'tokens_hour_data'
 
+FACTORY_ADDRESS = format_address(os.environ.get('FACTORY_CONTRACT'))
+NFT_ROUTER = format_address(os.environ.get('NFT_ROUTER_CONTRACT'))
+
 
 # mainnet contracts
-# should start with 0x0...
 if os.environ.get('NETWORK') == 'mainnet':
-    FACTORY_ADDRESS = '' # todo
-    NFT_ROUTER = '' # todo
-    
     ETH = '0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7'
     USDC = '0x53c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8'
-    DAI = '0x0da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3'
+    DAI = '0xda114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3'
     USDT = '0x68f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8'
     WBTC = '0x3fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac'
+    WSTETH = "0x42b8f0484674ca266ac5d08e4ac6a3fe65bd3129795def2dca5c34ecc5f96d2"
+    LORDS = "0x124aeb495b947201f5fac96fd1138e326ad86195b98df6dec9009158a533b49"
+    LUSD = "0x70a76fd48ca0ef910631754d77dd822147fe98a569b826ec85e3c33fde586ac"
 
-    ETH_USDC_ADDRESS = ''   # todo
+    ETH_USDC_ADDRESS = '0x7015a6822f109a2e41d25dd6878fe161ae9bb13eeb87e62de42a3158a64db28'
     WHITELISTED_POOLS = []  # todo
+    STABLECOINS = [USDC, USDT, DAI, LUSD]
+    WHITELISTED_TOKENS = [ETH, USDC, DAI, USDT, WBTC, WSTETH, LORDS, LUSD]
 else:
-    # testnet contracts
-    FACTORY_ADDRESS = '0x6b4115fa43c48118d3f79fbc500c75917c8a28d0f867479acb81893ea1e036c'
-    NFT_ROUTER = '0x0d61e6af51443e01bc62cc5ce2692c26ce7b3a228ded99b3c5cfc84ae3ac6a3'
-
     ETH = '0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7'
     USDC = '0x5a643907b9a4bc6a55e9069c4fd5fd1f5c79a22470690f75556c4736e34426'
     DAI = '0x3e85bfbb8e2a42b7bead9e88e9a1b19dbccf661471061807292120462396ec9'
@@ -48,10 +49,8 @@ else:
     WHITELISTED_POOLS = [
         '0x15b5061cab98c4b3a7d26cba1ae33636337c5bc8e17ba4cd9dcc5c9bab2c615',
     ]
-
-
-STABLECOINS = [USDC, USDT, DAI]
-WHITELISTED_TOKENS = [ETH, USDC, DAI, USDT, WBTC]
+    STABLECOINS = [USDC, USDT, DAI]
+    WHITELISTED_TOKENS = [ETH, USDC, DAI, USDT, WBTC]
 
 ZERO_ADDRESS = '0x0000000000000000000000000000000000000000000000000000000000000000'
 ZERO_DECIMAL128 = Decimal128(Decimal(0))
